fd8ba965770c74105aac8a64c35c8116308fe2a0
spyder1211 <soichiro.yano.g@gmail.com>
2024-07-22 20:11:49 +0900

    feat: Add web scraping functionality to extract links from a webpage
    This commit adds code to scrape a webpage and extract links using the BeautifulSoup library. The extracted links are stored in a list for further processing. The code uses the requests library to make HTTP requests and the BeautifulSoup library to parse the HTML content of the webpage.
    
    The extracted links are then displayed and used to load documents using the WebBaseLoader. The loaded documents are split into smaller text segments using the RecursiveCharacterTextSplitter. Finally, the documents are indexed using the FAISS library and the BedrockEmbeddings.
    
    This commit also updates the chat chain to include the retrieval of relevant documents based on the user's question. The updated chain now includes the format_docs function to extract the page content and source metadata from the loaded documents.
    
    The changes in this commit enhance the functionality of the application by enabling web scraping and document retrieval capabilities.

--- main.py
+++ main.py
@@ -1,15 +1,77 @@
+import requests
+import bs4
+from bs4 import BeautifulSoup
 from fastapi import FastAPI
+from langchain_community.document_loaders import WebBaseLoader
 from langchain_core.prompts import ChatPromptTemplate
 from langchain_core.output_parsers import StrOutputParser
 from langchain_aws import ChatBedrock
 from langserve import add_routes
 from mangum import Mangum
+from langchain_aws import ChatBedrock, BedrockEmbeddings
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain_community.vectorstores import FAISS
+from langchain import hub
+from langchain.chains.combine_documents import create_stuff_documents_chain
+from langchain.chains import create_retrieval_chain
+from pydantic import BaseModel
+from langchain_core.runnables import RunnablePassthrough
 
-# 1. Create prompt template
-prompt = ChatPromptTemplate.from_messages([
-    ('system',"Hello, I'm a chatbot. What's your name? conversation in japanese"),
-    ('user',"My name is {name}"),
-])
+url_list = [
+    "https://applion.jp/android/rank/us/6014/",
+    # "https://applion.jp/android/rank/us/6014/?start=20",
+    # "https://applion.jp/android/rank/us/6014/?start=40",
+    # "https://applion.jp/android/rank/us/6014/?start=60",
+    # "https://applion.jp/android/rank/us/6014/?start=80",
+    ]
+
+# 抽出したリンクを格納するリスト
+extracted_links = []
+
+# User-Agentヘッダーを追加
+headers = {
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36'
+}
+
+for url in url_list:
+    # URLの内容を取得
+    response = requests.get(url, headers=headers)
+    
+    # BeautifulSoupオブジェクトを作成
+    soup = BeautifulSoup(response.content, 'html.parser')
+    
+    # 'rownormal'クラスを持つ要素を検索
+    elements = soup.find_all(class_='rownormal')
+
+    # 各要素内のリンクを抽出
+    for element in elements:
+        links = element.find_all('a', href=True)
+        for link in links:
+            # リンクが相対パスの場合は絶対パスに変換
+            if link['href'].startswith('/'):
+                extracted_links.append('https://applion.jp' + link['href'])
+            else:
+                extracted_links.append(link['href'])
+
+docs = []
+
+# 抽出したリンクを表示
+for link in extracted_links:
+    print(link)
+    loader = WebBaseLoader(
+        link,
+        bs_kwargs=dict(
+            parse_only=bs4.SoupStrainer('div', class_='h1_title'),
+        ),
+    )
+    docs.extend(loader.load())
+    print(docs)
+
+text_splitter = RecursiveCharacterTextSplitter()
+documents = text_splitter.split_documents(docs)
+index = FAISS.from_documents(documents, BedrockEmbeddings())
+
+prompt = hub.pull("rlm/rag-prompt")
 
 # 2. Create model
 model = ChatBedrock(
@@ -17,8 +79,17 @@ model = ChatBedrock(
     model_id='anthropic.claude-3-haiku-20240307-v1:0',
 )
 
+def format_docs(docs):
+    # docsのpage_contentと、metadataのsourceを返す
+    return [doc.page_content for doc in docs], [doc.metadata['source'] for doc in docs]
+
 # 3. Create chain
-chain = prompt | model | StrOutputParser()
+rag_chain = (
+    {"context": index.as_retriever() | format_docs, "question": RunnablePassthrough()}
+    | prompt
+    | model
+    | StrOutputParser()
+)
 
 # 4. Create FastAPI app
 app = FastAPI(
@@ -33,7 +104,7 @@ handler = Mangum(app)
 # 6. Add routes
 add_routes(
     app, 
-    chain,
+    rag_chain,
     path='/chat',
 )
 
